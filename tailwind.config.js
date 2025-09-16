a91f99c2e1973f8bf78c44f3ac1d7d0a1dd010b3
d4mie <damio.ogunnaike@gmail.com>
2025-09-16 10:11:10 +0100

    push

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'nothingyoucoulddo': ['Nothing You Could Do', 'cursive'],
+        'signika': ['Signika', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
+
