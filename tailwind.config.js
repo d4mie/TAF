8bce72e26adc30eba6b40b62bae151cc1fac8976
d4mie <damio.ogunnaike@gmail.com>
2026-08-11 21:13:12 +0100

    Use Montserrat site-wide and add a light/dark theme toggle.
    Unifies typography with the VISIONARY look and lets visitors pick and remember their preferred color mode.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
-        'nothingyoucoulddo': ['Nothing You Could Do', 'cursive'],
-        'signika': ['Signika', 'sans-serif'],
+        nothingyoucoulddo: ["Nothing You Could Do", "cursive"],
+        signika: ["Montserrat", "system-ui", "sans-serif"],
+        montserrat: ["Montserrat", "system-ui", "sans-serif"],
+        sans: ["Montserrat", "system-ui", "sans-serif"],
+        serif: ["Montserrat", "system-ui", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
-
-
+};
